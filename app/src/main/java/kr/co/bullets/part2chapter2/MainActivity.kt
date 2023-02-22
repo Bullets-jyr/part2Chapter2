ef359e05f3aa190b936122e4889ea45c8aa11d60
Bullets-jyr <jyr@bullets.co.kr>
2023-02-22 11:15:39 +0900

    02. 기본 UI 구성하기

--- app/src/main/java/kr/co/bullets/part2chapter2/MainActivity.kt
+++ app/src/main/java/kr/co/bullets/part2chapter2/MainActivity.kt
@@ -2,10 +2,14 @@ package kr.co.bullets.part2chapter2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.bullets.part2chapter2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }

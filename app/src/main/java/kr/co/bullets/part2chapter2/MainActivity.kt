e033cf6eb9c3e2d3430c6b63b01578deb4f96ab7
Bullets-jyr <jyr@bullets.co.kr>
2023-02-22 16:37:44 +0900

    05. 재생 기능 구현하기

--- app/src/main/java/kr/co/bullets/part2chapter2/MainActivity.kt
+++ app/src/main/java/kr/co/bullets/part2chapter2/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.content.res.ColorStateList
 import android.graphics.Color
+import android.media.MediaPlayer
 import android.media.MediaRecorder
 import android.net.Uri
 import android.os.Build
@@ -34,6 +35,8 @@ class MainActivity : AppCompatActivity() {
 
     private var recorder: MediaRecorder? = null
 
+    private var player: MediaPlayer? = null
+
     private var fileName: String = ""
 
     private var state: State = State.RELEASE
@@ -61,6 +64,40 @@ class MainActivity : AppCompatActivity() {
                     onRecord(false)
                 }
                 State.PLAYING -> {
+
+                }
+            }
+        }
+
+        binding.playButton.setOnClickListener {
+            when (state) {
+                State.RELEASE -> {
+                    onPlay(true)
+                }
+//                State.RECORDING -> {
+//
+//                }
+//                State.PLAYING -> {
+//
+//                }
+                else -> {
+
+                }
+            }
+        }
+
+        binding.stopButton.setOnClickListener {
+            when (state) {
+//                State.RELEASE -> {
+//
+//                }
+//                State.RECORDING -> {
+//
+//                }
+                State.PLAYING -> {
+                    onPlay(false)
+                }
+                else -> {
 
                 }
             }
@@ -101,6 +138,12 @@ class MainActivity : AppCompatActivity() {
         startRecording()
     } else {
         stopRecording()
+    }
+
+    private fun onPlay(start: Boolean) = if (start) {
+        startPlaying()
+    } else {
+        stopPlaying()
     }
 
     private fun startRecording() {
@@ -157,6 +200,38 @@ class MainActivity : AppCompatActivity() {
 
         binding.playButton.isEnabled = true
         binding.playButton.alpha = 1.0f
+    }
+
+    private fun startPlaying() {
+        state = State.PLAYING
+
+        player = MediaPlayer().apply {
+            try {
+                setDataSource(fileName)
+                prepare()
+            } catch (e: IOException) {
+                Log.e("MainActivity", "media player prepare fail $e")
+            }
+
+            start()
+        }
+
+        player?.setOnCompletionListener {
+            stopPlaying()
+        }
+
+        binding.recordButton.isEnabled = false
+        binding.recordButton.alpha = 0.3f
+    }
+
+    private fun stopPlaying() {
+        state = State.RELEASE
+
+        player?.release()
+        player = null
+
+        binding.recordButton.isEnabled = true
+        binding.recordButton.alpha = 1.0f
     }
 
     private fun showPermissionRationaleDialog() {
